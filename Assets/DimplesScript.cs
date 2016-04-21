f455a203920ff4a41d10b581af07b532ad64fa3a
Komran Ghahremani <gkomran@gmail.com>
2016-04-20 23:13:30 -0400

    Dimples Triggers

---
+++ Assets/DimplesScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DimplesScript : MonoBehaviour {
+	public Transform target;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bool leftPunch = Input.GetKey (KeyCode.Y);
+		bool rightPunch = Input.GetKey (KeyCode.U);
+		bool death = Input.GetKey (KeyCode.P);
+		bool getHit1 = Input.GetKey (KeyCode.O);
+		bool getHit2 = Input.GetKey (KeyCode.M);
+
+		if (leftPunch) {
+			this.GetComponent<Animator> ().SetTrigger ("punchL");
+		}
+
+		if (rightPunch) {
+			this.GetComponent<Animator> ().SetTrigger ("punchR");
+		}
+
+		if (death) {
+			this.GetComponent<Animator> ().SetTrigger ("death");
+		}
+
+		if (getHit1) {
+			this.GetComponent<Animator> ().SetTrigger ("gethit");
+		}
+
+		if (getHit2) {
+			this.GetComponent<Animator> ().SetTrigger ("gethit2");
+		}
+	}
+}
