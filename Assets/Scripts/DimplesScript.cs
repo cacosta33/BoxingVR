b1026b016d9e0db338040478713b21b832ccb13a
Komran Ghahremani <gkomran@gmail.com>
2016-04-22 19:12:37 -0400

    Komran Up to Date

--- Assets/Scripts/DimplesScript.cs
+++ Assets/Scripts/DimplesScript.cs
@@ -6,7 +6,7 @@ public class DimplesScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public float maxHealth = 100f;
+	public float currentHealth = 0f;
+	public GameObject healthBar;
+
+	// Use this for initialization
+	void Start () {
+		currentHealth = maxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
